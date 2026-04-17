0b49a4b7ec174a0deb14bd80abfada4799edb656
Christian Kellner <weakmap@gmail.com>
2026-04-17 12:52:41 +0200

    showing log level

--- server/src/logger.js
+++ server/src/logger.js
@@ -7,6 +7,11 @@ import pino from 'pino'
 
 export const logger = pino({
   level: process.env.LOG_LEVEL || 'info',
+  formatters: {
+    level(label) {
+      return { level: label }
+    },
+  },
   transport:
     process.env.NODE_ENV !== 'production'
       ? { target: 'pino-pretty', options: { colorize: true, translateTime: 'SYS:HH:MM:ss', ignore: 'pid,hostname' } }
